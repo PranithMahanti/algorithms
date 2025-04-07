1ab6f220ac7c159df45398c40e31e54c272fb03f
PranithMahanti <74035516+PranithMahanti@users.noreply.github.com>
2025-04-07 09:50:29 +0530

    Add files via upload

--- basic-algs.py
+++ basic-algs.py
@@ -35,4 +35,39 @@ def selection_sort(list):
     
     return sorted_list
 
+# Divide and Conquer for sum of list items
+def dc_sum(list):
+    if not list:
+        return 0
+    
+    sum = list.pop(0) + dc_sum(list)
+    return sum
+
+# Divide and Conquer for maximum of list items
+def dc_max_run(list):
+    global max
+    max = 0
+    def dc_max(list):
+        global max
+        if list:
+            if list[0] >= max:
+                max = list.pop(0)
+                dc_max(list)
+            else:
+                list.pop(0)
+        return max
+    
+    return dc_max(list)
+
+
+# Quick Sort
+def quick_sort(list):
+    if len(list) < 2:
+        return list
+    
+    pivot = list[0]
+    lesser = [i for i in list if i<pivot]
+    greater = [i for i in list if i>pivot]
+
+    return quick_sort(lesser) + [pivot] + quick_sort(greater)
 
